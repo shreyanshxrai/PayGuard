2ac866241e7844fb6e56cba477d05f179ca8ded2
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-06 21:38:58 +0530

    schema change

--- paygaurd/packages/schemas/src/schema.ts
+++ paygaurd/packages/schemas/src/schema.ts
@@ -2,9 +2,9 @@ import {z} from "zod";
 import { email } from "zod/v4";
 
 const UserSchema = z.object({
-    name : z.string(),
+    name : z.string().optional(),
     email : z.string().email() ,
-    phone : z.number().max(10).min(10),
+    phone : z.number().max(10).min(10).optional(),
     password : z.string()
 
 })
