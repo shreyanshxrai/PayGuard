f124120c595f9efd92368b7323a6631ce70f9099
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-11 02:45:08 +0530

    format

--- paygaurd/apps/user-app/app/api/signin/route.ts
+++ paygaurd/apps/user-app/app/api/signin/route.ts
@@ -7,43 +7,47 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
 // post request for signin
-export async function POST(request : Request) {
-    let body;
-    try{
-        body = await request.json();
-
-    }
-    catch(err){
-        console.error(err);
-       return NextResponse.json({msg : "Error in the body"} , {status : 400})
-    }
-    const parseBody = siginSchema.safeParse(body);
-    if(parseBody.success){
-        const {email , password } = parseBody.data;
-        try{
-        const user = await prisma.user.findUnique({
-            where : {email : email} 
-        })
-        if(!user){
-            return NextResponse.json({msg : "Invalid email or Password"}  , {status : 401});
+export async function POST(request: Request) {
+  let body;
+  try {
+    body = await request.json();
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ msg: "Error in the body" }, { status: 400 });
+  }
+  const parseBody = siginSchema.safeParse(body);
+  if (parseBody.success) {
+    const { email, password } = parseBody.data;
+    try {
+      const user = await prisma.user.findUnique({
+        where: { email: email },
+      });
+      if (!user) {
+        return NextResponse.json(
+          { msg: "Invalid email or Password" },
+          { status: 401 },
+        );
+      } else {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          return NextResponse.json(
+            { msg: "Invalid email or Password" },
+            { status: 401 },
+          );
         }
-        else{
-            const isPasswordValid= await bcrypt.compare(password , user.password);
-            if(!isPasswordValid){
-                return NextResponse.json({msg:"Invalid email or Password"} , {status : 401});
-            }
-            const payload = {id : user.id , email : user.email};
-            const token = await generateToken(payload);
-            await setAuthCookie(token);
-            return NextResponse.json({msg : "Logged In"} , {status : 200} )
-        }
+        const payload = { id: user.id, email: user.email };
+        const token = await generateToken(payload);
+        await setAuthCookie(token);
+        return NextResponse.json({ msg: "Logged In" }, { status: 200 });
+      }
+    } catch (err) {
+      console.error(err);
+      return NextResponse.json(
+        { msg: "Authentication Error" },
+        { status: 400 },
+      );
     }
-    catch(err){
-        console.error(err);
-       return NextResponse.json({msg : "Authentication Error"} , {status : 400})
-    }
-    }
-    else{
-        return NextResponse.json({msg : "Bad inputs"} , {status : 400})
-    }
+  } else {
+    return NextResponse.json({ msg: "Bad inputs" }, { status: 400 });
+  }
 }
