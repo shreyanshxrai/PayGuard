cef4ea5b5ea62d44c8ce78602567c2c9ff2c4381
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-21 11:42:23 +0530

    Added functionality to get route

--- paygaurd/apps/user-app/app/api/balance/route.ts
+++ paygaurd/apps/user-app/app/api/balance/route.ts
@@ -1,1 +1,28 @@
 // get req for balance
+
+import { getAuthCookie } from "@/lib/auth/cookies";
+import { verifyToken } from "@/lib/auth/jwt";
+import { prisma } from "@repo/db";
+import { NextResponse } from "next/server";
+
+export async function GET(request : Request) {
+    try {
+        const token = await getAuthCookie() ;
+        const payload = await verifyToken(token);
+        if(payload){
+            const { id } = payload;
+            const userId = Number(id)
+            const balance = await prisma.user.findUnique({where : {id : userId} , select : {}})
+            if(balance){
+            return NextResponse.json(balance, {status : 200})}
+
+        }
+        else {
+            return NextResponse.json({msg : 'Unauthorized'},{status : 401});
+        }
+    }
+    catch(err){
+        console.error(err);
+        return NextResponse.json({msg : 'server error'} , {status : 500})
+    }
+}
