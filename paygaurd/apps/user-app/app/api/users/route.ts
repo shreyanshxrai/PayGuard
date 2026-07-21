ddea337fba09567ae0c7233794e075fbe97ec770
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-21 21:13:40 +0530

    added logic to users route

--- paygaurd/apps/user-app/app/api/users/route.ts
+++ paygaurd/apps/user-app/app/api/users/route.ts
@@ -1,1 +1,28 @@
+import { getAuthCookie } from "@/lib/auth/cookies";
+import { verifyToken } from "@/lib/auth/jwt";
+import { prisma } from "@repo/db";
+import { NextResponse } from "next/server";
+
 // get users list
+export async function GET() {
+  try {
+    const token = await getAuthCookie();
+    const payload = await verifyToken(token);
+    if (!payload) {
+      return NextResponse.json({ msg: "Unauthorized" }, { status: 401 });
+    }
+    const Users = await prisma.user.findMany({
+      select: { id: true, name: true },
+    });
+    if (Users.length === 0) {
+      return NextResponse.json({ msg: "No Users yet" }, { status: 404 });
+    }
+    return NextResponse.json(Users, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { msg: "error fetching history" },
+      { status: 500 },
+    );
+  }
+}
