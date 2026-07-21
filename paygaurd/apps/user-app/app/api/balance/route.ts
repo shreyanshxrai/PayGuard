e8a24c2040073269c8e32917319ed6809d9bf224
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-21 21:15:51 +0530

    format change

--- paygaurd/apps/user-app/app/api/balance/route.ts
+++ paygaurd/apps/user-app/app/api/balance/route.ts
@@ -5,24 +5,25 @@ import { verifyToken } from "@/lib/auth/jwt";
 import { prisma } from "@repo/db";
 import { NextResponse } from "next/server";
 
-export async function GET(request : Request) {
-    try {
-        const token = await getAuthCookie() ;
-        const payload = await verifyToken(token);
-        if(payload){
-            const { id } = payload;
-            const userId = Number(id)
-            const balance = await prisma.user.findUnique({where : {id : userId} , select : {balance : true}})
-            if(balance){
-            return NextResponse.json(balance, {status : 200})}
-
-        }
-        else {
-            return NextResponse.json({msg : 'Unauthorized'},{status : 401});
-        }
+export async function GET(request: Request) {
+  try {
+    const token = await getAuthCookie();
+    const payload = await verifyToken(token);
+    if (payload) {
+      const { id } = payload;
+      const userId = Number(id);
+      const balance = await prisma.user.findUnique({
+        where: { id: userId },
+        select: { balance: true },
+      });
+      if (balance) {
+        return NextResponse.json(balance, { status: 200 });
+      }
+    } else {
+      return NextResponse.json({ msg: "Unauthorized" }, { status: 401 });
     }
-    catch(err){
-        console.error(err);
-        return NextResponse.json({msg : 'server error'} , {status : 500})
-    }
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ msg: "server error" }, { status: 500 });
+  }
 }
--- paygaurd/apps/user-app/app/api/history/route.ts
+++ paygaurd/apps/user-app/app/api/history/route.ts
@@ -3,30 +3,30 @@ import { verifyToken } from "@/lib/auth/jwt";
 import { prisma } from "@repo/db";
 import { NextResponse } from "next/server";
 
-
 // get request for payment history
-export async function GET(request : Request) {
-    try{
+export async function GET(request: Request) {
+  try {
     const token = await getAuthCookie();
     const payload = await verifyToken(token);
-    if(payload){
-    const { id , email } = payload
-    const fromUserId = Number(id)
-    const history = await prisma.transactionHistory.findMany({ where : {fromUserId : fromUserId}})
-    if(history.length === 0){
-        return NextResponse.json({msg : "No transactions"} , {status : 404})
-    }
-   
-        return NextResponse.json(history , {status : 200})
-    
-}
-else{
-    return NextResponse.json({msg : "Unauthorized"} , {status : 401})
-}
-}
-    catch(err) {
-        console.error(err);
-        return NextResponse.json({msg : "error fetching history"} , {status : 500})
-    }
+    if (payload) {
+      const { id, email } = payload;
+      const fromUserId = Number(id);
+      const history = await prisma.transactionHistory.findMany({
+        where: { fromUserId: fromUserId },
+      });
+      if (history.length === 0) {
+        return NextResponse.json({ msg: "No transactions" }, { status: 404 });
+      }
 
+      return NextResponse.json(history, { status: 200 });
+    } else {
+      return NextResponse.json({ msg: "Unauthorized" }, { status: 401 });
+    }
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { msg: "error fetching history" },
+      { status: 500 },
+    );
+  }
 }
