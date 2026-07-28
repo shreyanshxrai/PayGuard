b855bdd98b9669f5a439ac853a39690ba26fa034
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-28 10:07:59 +0530

    Added express server and few bug fixes

--- paygaurd/apps/express-server/src/index.ts
+++ paygaurd/apps/express-server/src/index.ts
@@ -1,1 +1,13 @@
 //server express
+import express , { type Express, type Request, type Response } from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
