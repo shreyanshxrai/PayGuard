770874312dced3bb10cf106ae944dd04bfe33a80
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-28 10:30:50 +0530

    updated

--- paygaurd/apps/express-server/src/index.ts
+++ paygaurd/apps/express-server/src/index.ts
@@ -1,8 +1,13 @@
 //server express
 import express , { type Express, type Request, type Response } from 'express';
+import cors from 'cors'
 
 const app: Express = express();
+const router = express.Router()
 const port = 3000;
+express.json();
+
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
