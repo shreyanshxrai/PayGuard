1de67000c8379b430adb1d8c0219cad371ef1347
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-30 13:37:36 +0530

    made transaction service functional

--- paygaurd/apps/express-server/src/services/transaction.service.ts
+++ paygaurd/apps/express-server/src/services/transaction.service.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@repo/db";
 interface SendMoneyInput {
   senderId: number;
   receiverId: number;
@@ -6,18 +7,51 @@ interface SendMoneyInput {
 
 export async function transactionService(data: SendMoneyInput) {
   const { senderId, receiverId, amount } = data;
+  try {
+    if (amount <= 0) {
+      console.error("Amount must be greater than zero");
+      throw new Error("Amount must be greater than zero");
+    }
+    const sender = await prisma.user.findUnique({
+      where: { id: senderId },
+    });
+    if (!sender) {
+      throw new Error("Sender not found");
+    }
 
-  console.log({
-    senderId,
-    receiverId,
-    amount,
-  });
+    const receiver = await prisma.user.findUnique({
+      where: { id: receiverId },
+    });
+    if (!receiver) {
+      throw new Error("Receiver not found");
+    }
+    if (senderId === receiverId) {
+      throw new Error("Sender and receiver cannot be the same");
+    }
 
-  return {
-    transactionId: "TEMP_ID",
-    senderId,
-    receiverId,
-    amount,
-    status: "SUCCESS",
-  };
+    const transaction = await prisma.$transaction(async (prisma) => {
+      const balance = sender?.balance;
+      if (balance === undefined || balance < amount) {
+        throw new Error("Insufficient balance");
+      }
+      const updatedSender = await prisma.user.update({
+        where: { id: senderId },
+        data: { balance: { decrement: amount } },
+      });
+      const updatedReceiver = await prisma.user.update({
+        where: { id: receiverId },
+        data: { balance: { increment: amount } },
+      });
+      const history = await prisma.transactionHistory.create({
+        data: {
+          fromUserId: senderId,
+          toUserId: receiverId,
+          amount: amount,
+        },
+      });
+      return console.log("Transaction completed successfully");
+    });
+  } catch (error) {
+    throw new Error(`Transaction failed: ${error}`);
+  }
 }
