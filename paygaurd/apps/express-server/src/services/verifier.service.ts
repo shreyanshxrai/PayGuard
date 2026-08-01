15e556c2669cece3bfc6c24882f26265c65a2fd7
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-08-02 01:46:26 +0530

    added functionality to verifier service file

--- paygaurd/apps/express-server/src/services/verifier.service.ts
+++ paygaurd/apps/express-server/src/services/verifier.service.ts
@@ -12,3 +12,38 @@ interface VerificationResult {
 
   triggeredRules: number;
 }
+
+export async function verifyTransaction(
+  transactionAmount: number,
+  senderId: number,
+  receiverId: number,
+): Promise<VerificationResult> {
+  const context = {
+    amount: transactionAmount,
+    senderId,
+    receiverId,
+    createdAt: new Date(),
+  };
+  const rules = await Promise.all([
+    largeAmountRule(context),
+    nightTimeRule(),
+    multipleTransactionRule(context),
+    newRecipientRule(context),
+  ]);
+  return {
+    totalScore: rules.reduce((acc, rule) => acc + rule.score, 0),
+    reasons: rules.flatMap((rule) => rule.reason),
+    riskLevel:
+      rules.reduce((acc, rule) => acc + rule.score, 0) >
+      RULE_CONFIG.HIGH_RISK_SCORE
+        ? "CRITICAL"
+        : rules.reduce((acc, rule) => acc + rule.score, 0) ==
+            RULE_CONFIG.HIGH_RISK_SCORE
+          ? "HIGH"
+          : rules.reduce((acc, rule) => acc + rule.score, 0) >
+              RULE_CONFIG.MEDIUM_RISK_SCORE
+            ? "MEDIUM"
+            : "LOW",
+    triggeredRules: rules.filter((rule) => rule.score > 0).length,
+  };
+}
