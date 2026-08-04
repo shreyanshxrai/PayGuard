f04bc1a1ea87efa3a3b042752d6a5c53c0f0445e
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-08-04 15:06:04 +0530

    added request logger middleware

--- paygaurd/apps/express-server/src/index.ts
+++ paygaurd/apps/express-server/src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import transactionRouter from "./routes/transaction.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import { requestLogger } from "./middleware/requestLogger.js";
 
 const app = express();
 
 app.use(cors());
-
+app.use(requestLogger);
 app.use(express.json());
+app.use(errorHandler);
 
 app.get("/health", (_, res) => {
   res.status(200).json({
@@ -15,7 +17,7 @@ app.get("/health", (_, res) => {
     message: "PayGuard Express API is running",
   });
 });
-app.use(errorHandler);
+
 app.use("/transaction", transactionRouter);
 
 const PORT = process.env.PORT || 3001;
