68502c1b5d0dd03229ffbdb91c419c04d3cff0e5
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-08-20 19:07:33 +0530

    added a get user route

---
+++ paygaurd/apps/user-app/app/api/users/me/route.ts
@@ -0,0 +1,28 @@
+import { getAuthCookie } from "@/lib/auth/cookies";
+import { verifyToken } from "@/lib/auth/jwt";
+import { prisma } from "@repo/db";
+import { NextResponse } from "next/server";
+
+// get users list
+export async function GET() {
+  try {
+    const token = await getAuthCookie();
+    const payload = await verifyToken(token);
+    if (!payload) {
+      return NextResponse.json({ msg: "Unauthorized" }, { status: 401 });
+    }
+    const { id } = payload;
+    const userId = Number(id);
+    const Users = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { id: true, name: true, email: true, phone: true },
+    });
+    return NextResponse.json(Users, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { msg: "error fetching history" },
+      { status: 500 },
+    );
+  }
+}
